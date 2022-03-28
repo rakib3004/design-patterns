f89d820e2ba4d0fbe318106e21c03ebbc0bb257a
rakib3004 <bsse1129@iit.du.ac.bd>
2022-03-28 09:24:28 +0600

    add signleton pattern

--- src/DesignPatternImplementation/BehaviouralPatterns/SingletonPattern/Singleton.java
+++ src/DesignPatternImplementation/BehaviouralPatterns/SingletonPattern/Singleton.java
@@ -1,4 +1,89 @@
-package DesignPatternImplementation.BehaviouralPatterns;
+package DesignPatternImplementation.BehaviouralPatterns.SingletonPattern;
 
-public class RocketEnemyShip extends EnemyShip {
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+
+public class Singleton {
+	
+	private static Singleton firstInstance = null;
+	
+	String[] scrabbleLetters = {"a", "a", "a", "a", "a", "a", "a", "a", "a",
+			"b", "b", "c", "c", "d", "d", "d", "d", "e", "e", "e", "e", "e", 
+			"e", "e", "e", "e", "e", "e", "e", "f", "f", "g", "g", "g", "h", 
+			"h", "i", "i", "i", "i", "i", "i", "i", "i", "i", "j", "k", "l", 
+			"l", "l", "l", "m", "m", "n", "n", "n", "n", "n", "n", "o", "o", 
+			"o", "o", "o", "o", "o", "o", "p", "p", "q", "r", "r", "r", "r", 
+			"r", "r", "s", "s", "s", "s", "t", "t", "t", "t", "t", "t", "u", 
+			"u", "u", "u", "v", "v", "w", "w", "x", "y", "y", "z",};  
+	
+	private LinkedList<String> letterList = new LinkedList<String> (Arrays.asList(scrabbleLetters));
+   
+	static boolean firstThread = true;
+	
+
+   
+	private Singleton() { }
+	
+
+	
+	public static Singleton getInstance() {
+		if(firstInstance == null) {
+			
+			
+			
+			if(firstThread){
+			
+				firstThread = false;
+				
+				try {
+					Thread.currentThread();
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+				
+					e.printStackTrace();
+				}
+			}
+			
+			
+			synchronized(Singleton.class){ 
+			
+				if(firstInstance == null) {
+			
+					firstInstance = new Singleton();
+			
+					Collections.shuffle(firstInstance.letterList);
+					
+				}
+			
+			}
+			
+		}
+		
+		
+		return firstInstance;
+	}
+	
+	public LinkedList<String> getLetterList(){
+		
+		return firstInstance.letterList;
+		
+	}
+	
+	public LinkedList<String> getTiles(int howManyTiles){
+		
+		
+		LinkedList<String> tilesToSend = new LinkedList<String>();
+		
+		for(int i = 0; i <= howManyTiles; i++){
+		
+			tilesToSend.add(firstInstance.letterList.remove(0));
+		
+		}
+		
+		
+		return tilesToSend;
+		
+	}
+	
 }
