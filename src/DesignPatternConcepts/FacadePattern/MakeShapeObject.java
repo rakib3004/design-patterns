d9f7cce96be56948a12b340cf44a2f46ef83d757
rakib3004 <bsse1129@iit.du.ac.bd>
2022-04-07 18:04:52 +0600

    draw all shape

---
+++ src/DesignPatternConcepts/FacadePattern/MakeShapeObject.java
@@ -0,0 +1,11 @@
+package DesignPatternConcepts.FacadePattern;
+
+public class MakeShapeObject {
+    public static void main(String[] args) {
+        ShapeMaker shapeMaker = new ShapeMaker();
+
+        shapeMaker.drawCircle();
+        shapeMaker.drawRectangle();
+        shapeMaker.drawSquare();
+    }
+}
