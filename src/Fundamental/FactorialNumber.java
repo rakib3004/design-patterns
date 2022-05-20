2402c37999a153ca7cdeb6934cc24adc66cfdc1a
rakib3004 <bsse1129@iit.du.ac.bd>
2022-05-20 14:57:58 +0600

    add new factorial number

---
+++ src/Fundamental/FactorialNumber.java
@@ -0,0 +1,25 @@
+package Fundamental;
+
+import java.util.Scanner;
+
+public class FactorialNumber {
+
+    public static void main(String[] args) {
+        ;
+        int factorialResult=1;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the Number: ");
+        int factorialNumber=scanner.nextInt();
+
+
+        int iterator=0;
+
+        for(iterator=1;iterator<=factorialNumber;iterator++){
+            factorialResult=factorialResult*iterator;
+
+        }
+        System.out.println("The factorial of "+factorialNumber+ " is : "+factorialResult);
+    }
+
+}
