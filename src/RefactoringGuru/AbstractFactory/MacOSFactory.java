9628870bda73b3f604cd11db25b1e2f0f8833679
rakib3004 <bsse1129@iit.du.ac.bd>
2022-05-21 00:39:33 +0600

    add mac and windows factory

---
+++ src/RefactoringGuru/AbstractFactory/MacOSFactory.java
@@ -0,0 +1,14 @@
+package RefactoringGuru.AbstractFactory;
+
+public class MacOSFactory implements GUIFactory {
+
+    @Override
+    public Button createButton() {
+        return new MacOSButton();
+    }
+
+    @Override
+    public Checkbox createCheckbox() {
+        return new MacOSCheckbox();
+    }
+}
---
+++ src/RefactoringGuru/AbstractFactory/WindowsFactory.java
@@ -0,0 +1,14 @@
+package RefactoringGuru.AbstractFactory;
+
+public class WindowsFactory implements GUIFactory {
+
+    @Override
+    public Button createButton() {
+        return new WindowsButton();
+    }
+
+    @Override
+    public Checkbox createCheckbox() {
+        return new WindowsCheckbox();
+    }
+}
