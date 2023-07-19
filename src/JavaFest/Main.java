f77b0c1c5094f997bb0e6d20a47f172b87423f64
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2023-07-19 22:34:11 +0600

    add sample code for practicing code in javafest

---
+++ src/JavaFest/Main.java
@@ -0,0 +1,12 @@
+package JavaFest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+       String s = new String("ByCycle");
+        System.out.println(s.charAt(4));
+    }
+}
+
